daa48ce059a3d5222a50fa2f79b30d524408fe53
RuudyLee <rudy.lee.318@gmail.com>
2017-02-02 22:12:10 -0500

    Added script for stabilizing pod. Also increased gravity, and revised some pod parameters.

--- Assets/Scripts/PodEngine.cs
+++ Assets/Scripts/PodEngine.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PodEngine : MonoBehaviour
 {
-    public float speed = 5f;
-    public float lurkSpeed = 0.2f;
+    public float speed = 50000f;
+    public float lurkSpeed = 10000f;
+    public CapsuleCollider bodyCollider;
 
     ControllerPositionInput cpi;
     Rigidbody rb;
     public bool left = true;
-
+   
     // Use this for initialization
     void Start()
     {
@@ -21,10 +22,27 @@ public class PodEngine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Spherecast downwards
+        RaycastHit hitInfoDown;
+        if (Physics.SphereCast(transform.position,
+                               bodyCollider.radius,
+                               Vector3.down,
+                               out hitInfoDown,
+                               ((bodyCollider.height / 2f) - bodyCollider.radius) + 0.01f)) // add a small amount to help the spherecast
+        {
+            if (Mathf.Abs(Vector3.Angle(hitInfoDown.normal, Vector3.up)) < 85f)
+            {
+                rb.velocity = Vector3.ProjectOnPlane(rb.velocity, hitInfoDown.normal);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
         // Add a constant force forwards, feels more natural this way
         if (cpi.positionInput.left > 0 || cpi.positionInput.right > 0)
         {
-            rb.AddForce(transform.up * speed * Time.deltaTime);
+            rb.AddForce(transform.forward * speed * Time.deltaTime);
         }
 
         // Read inputs and apply force accordingly
@@ -37,6 +55,6 @@ public class PodEngine : MonoBehaviour
         {
             force = cpi.positionInput.right;
         }
-        rb.AddForce(transform.up * force * speed * Time.deltaTime);
+        rb.AddForce(transform.forward * force * speed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/PodStabilizer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodStabilizer : MonoBehaviour
+{
+    public float stability = 0.02f;
+
+    float xRotationLimit = 20f;
+    float yRotationLimit = 20f;
+    float zRotationLimit = 20f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 2.0f))
+        {
+            Vector3 groundNormal = hit.normal;
+            Vector3 probablyForward = Vector3.Cross(transform.right, groundNormal);
+            Quaternion probableDirection = Quaternion.LookRotation(probablyForward, groundNormal);
+
+            if (transform.rotation.eulerAngles.x > xRotationLimit)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, probableDirection, stability);
+            }
+
+            if (transform.rotation.eulerAngles.y > yRotationLimit)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, probableDirection, stability);
+            }
+
+            if (transform.rotation.eulerAngles.z > zRotationLimit)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, probableDirection, stability);
+            }
+        }
+
+
+    }
+}
+
