9bd48d8b267a9a92a4fba8fddeb33311af8a63d1
ishraf222 <100520689@oncampus.local>
2017-03-07 12:37:53 -0500

    so ammy took a free asset and took the turbines from it and thats whats gona be our turine, im working on a checkpoint system at the moment slow progress.

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+
+    
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider player)
+    {
+        this.gameObject.SetActive(false);
+        
+    }
+}
---
+++ Assets/Scripts/CheckPointManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointManager : MonoBehaviour {
+
+    public GameObject[] cp; //holds all check points
+
+    int activeCP = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+        {
+            activeCP = 0;
+
+            cp[activeCP].SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
